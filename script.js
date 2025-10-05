3df21992b39f3cd2f468cc34aab68b767ca6f405
Sereda Vitaliy <sereda.v484@gmail.com>
2025-10-06 01:15:14 +0200

    Реализация показа соседней строны

--- script.js
+++ script.js
@@ -8,22 +8,52 @@ const countriesContainer = document.querySelector('.countries');
 
 // NEW REVERSE GEOCODING API URL (use instead of the URL shown in videos):
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest(); //далее нам нужжен url по которому будем выполнять вызов ajax
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`); //1 - тип запроса(для получения данных get, 2 - строка, содержащаяь url адрес, по которому должен быть выполнен вызов ajax)
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest(); //далее нам нужжен url по которому будем выполнять вызов ajax
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`); //1 - тип запроса(для получения данных get, 2 - строка, содержащаяь url адрес, по которому должен быть выполнен вызов ajax)
 
-  //отправление запроса
-  request.send();
+//   //отправление запроса
+//   request.send();
 
-  //регистрация обратного вызова объекта запроса для события загрузки
-  request.addEventListener('load', function () {
-    //   console.log(this.responseText);
+//   //регистрация обратного вызова объекта запроса для события загрузки
+//   request.addEventListener('load', function () {
+//     //   console.log(this.responseText);
 
-    //преобразование json в реальный объект js
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//     //преобразование json в реальный объект js
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `<article class="country">
+//     const html = `<article class="country">
+//           <img class="country__img" src="${data.flags.svg}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)} people</p>
+//             <p class="country__row"><span>🗣️</span>${
+//               Object.values(data.languages)[0]
+//             }</p>
+//             <p class="country__row"><span>💰</span>${
+//               Object.values(data.currencies)[0].name
+//             }</p>
+//           </div>
+//         </article>`;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('russia');
+// // getCountryData('usa');
+// // getCountryData('germany');
+// // getCountryData('kazakhstan');
+
+// //создание последовательности вызовов. Чтобы второй вызов запускался строго после второго
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -40,12 +70,42 @@ const getCountryData = function (country) {
           </div>
         </article>`;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  //AJAX вызывает страну 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //отображение страны 1
+    renderCountry(data);
+
+    //соседняя страна 2
+    const [neighbour] = data.borders;
+
+    //для стран без совместной границы
+    if (!neighbour) return;
+
+    //AJAX вызывает страну 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      //   console.log(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('russia');
-getCountryData('usa');
-getCountryData('germany');
-getCountryData('kazakhstan');
+getCountryAndNeighbour('russia');
